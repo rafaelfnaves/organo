06fb202657154530645fd5baca56ed88de5ec422
Rafael Naves <r.naves21@gmail.com>
2023-03-30 20:34:54 -0300

    Changed header and footer at worker component

--- src/components/Worker/index.js
+++ src/components/Worker/index.js
@@ -3,10 +3,10 @@ import "./Worker.css"
 const Worker = ({name, role, image, cardColor}) => {
     return (
         <div className="worker">
-            <div className="header" style={{ backgroundColor: cardColor }}>
+            <div className="header-worker" style={{ backgroundColor: cardColor }}>
                 <img src={image} alt={name} />
             </div>
-            <div className="footer">
+            <div className="footer-worker">
                 <h4>{name}</h4>
                 <h5>{role}</h5>
             </div>
